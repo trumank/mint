4b1823b2dbe107a33cf81bfff53da54bc3124827
Truman Kilen <t@kilen.me>
2023-03-13 17:05:16 -0500

    Add caching to HttpProvider

--- src/providers/http.rs
+++ src/providers/http.rs
@@ -1,10 +1,14 @@
+use std::collections::HashMap;
 use std::io::Cursor;
 use std::sync::Arc;
 
 use anyhow::{anyhow, Result};
+use serde::{Deserialize, Serialize};
 use tokio::sync::RwLock;
 
-use super::{BlobCache, CacheWrapper, ModProvider, ModResponse, ResolvableStatus};
+use super::{
+    BlobCache, BlobRef, CacheWrapper, ModProvider, ModProviderCache, ModResponse, ResolvableStatus,
+};
 
 inventory::submit! {
     super::ProviderFactory {
@@ -18,6 +22,23 @@ inventory::submit! {
                     !["mod.io", "drg.mod.io", "drg.old.mod.io"].contains(&h.as_str())
                 })
         }
+    }
+}
+
+#[derive(Debug, Default, Serialize, Deserialize)]
+pub struct HttpProviderCache {
+    url_blobs: HashMap<String, BlobRef>,
+}
+#[typetag::serde]
+impl ModProviderCache for HttpProviderCache {
+    fn new() -> Self {
+        Default::default()
+    }
+    fn as_any(&self) -> &dyn std::any::Any {
+        self
+    }
+    fn as_any_mut(&mut self) -> &mut dyn std::any::Any {
+        self
     }
 }
 
@@ -46,27 +67,48 @@ impl ModProvider for HttpProvider {
     async fn get_mod(
         &self,
         url: &str,
-        _cache: Arc<RwLock<CacheWrapper>>,
-        _blob_cache: &BlobCache,
+        cache: Arc<RwLock<CacheWrapper>>,
+        blob_cache: &BlobCache,
     ) -> Result<ModResponse> {
-        println!("downloading mod {url}...");
-        Ok(ModResponse::Resolve {
-            status: ResolvableStatus::Resolvable {
-                url: url.to_owned(),
-            },
-            data: Box::new(Cursor::new({
-                let res = self.client.get(url).send().await?.error_for_status()?;
-                if let Some(mime) = res
-                    .headers()
-                    .get(reqwest::header::HeaderName::from_static("content-type"))
-                {
-                    let content_type = &mime.to_str()?;
-                    if !["application/zip", "application/octet-stream"].contains(content_type) {
-                        return Err(anyhow!("unexpected content-type: {content_type}"));
+        let mut wrapper = cache.write().await;
+        let cache = wrapper.get_mut::<HttpProviderCache>("http");
+        if let Some(blob) = cache
+            .url_blobs
+            .get(url)
+            .and_then(|r| blob_cache.read(r).ok())
+        {
+            Ok(ModResponse::Resolve {
+                status: ResolvableStatus::Resolvable {
+                    url: url.to_owned(),
+                },
+                data: blob,
+            })
+        } else {
+            println!("downloading mod {url}...");
+            Ok(ModResponse::Resolve {
+                status: ResolvableStatus::Resolvable {
+                    url: url.to_owned(),
+                },
+                data: Box::new(Cursor::new({
+                    let res = self.client.get(url).send().await?.error_for_status()?;
+                    if let Some(mime) = res
+                        .headers()
+                        .get(reqwest::header::HeaderName::from_static("content-type"))
+                    {
+                        let content_type = &mime.to_str()?;
+                        if !["application/zip", "application/octet-stream"].contains(content_type) {
+                            return Err(anyhow!("unexpected content-type: {content_type}"));
+                        }
                     }
-                }
-                res.bytes().await?.to_vec()
-            })),
-        })
+
+                    let data = res.bytes().await?.to_vec();
+                    cache
+                        .url_blobs
+                        .insert(url.to_owned(), blob_cache.write(&data)?);
+
+                    data
+                })),
+            })
+        }
     }
 }
