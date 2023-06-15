7d075c44f5be42bb89f0204663d4e7537cc865d8
Truman Kilen <t@kilen.me>
2023-06-15 17:55:56 -0500

    dry integration

--- src/lib.rs
+++ src/lib.rs
@@ -4,9 +4,99 @@ pub mod integrate;
 pub mod providers;
 pub mod state;
 
-use std::path::PathBuf;
+use std::{
+    collections::HashSet,
+    path::{Path, PathBuf},
+};
+
+use anyhow::Result;
+
+use error::IntegrationError;
+use providers::{ModResolution, ModSpecification, ProviderFactory, ResolvableStatus};
+use state::State;
 
 pub fn find_drg() -> Option<PathBuf> {
     steamlocate::SteamDir::locate()
         .and_then(|mut steamdir| steamdir.app(&548430).map(|a| a.path.to_path_buf()))
+}
+
+pub async fn resolve_and_integrate<P: AsRef<Path>>(
+    path_game: P,
+    state: &State,
+    mod_specs: &[ModSpecification],
+    update: bool,
+) -> Result<()> {
+    let mods = state.store.resolve_mods(mod_specs, update).await?;
+
+    println!("resolvable mods:");
+    for m in mod_specs {
+        if let ResolvableStatus::Resolvable(resolution) = &mods[m].status {
+            println!("{:?}", resolution);
+        }
+    }
+
+    let mods_set = mod_specs
+        .iter()
+        .flat_map(|m| match &mods[m].status {
+            ResolvableStatus::Resolvable(ModResolution { url }) => Some(url),
+            _ => None,
+        })
+        .collect::<HashSet<_>>();
+
+    let missing_deps = mod_specs
+        .iter()
+        .flat_map(|m| {
+            mods[m]
+                .suggested_dependencies
+                .iter()
+                .filter_map(|m| match &mods[m].status {
+                    ResolvableStatus::Resolvable(ModResolution { url }) => {
+                        (!mods_set.contains(url)).then_some(url)
+                    }
+                    _ => Some(&m.url),
+                })
+        })
+        .collect::<HashSet<_>>();
+    if !missing_deps.is_empty() {
+        println!("WARNING: The following dependencies are missing:");
+        for d in missing_deps {
+            println!("  {d}");
+        }
+    }
+
+    let to_integrate = mod_specs
+        .iter()
+        .map(|u| mods[u].clone())
+        .collect::<Vec<_>>();
+    let urls = to_integrate
+        .iter()
+        .map(|m| &m.spec) // TODO this should be a ModResolution not a ModSpecification, we're missing a step here
+        .collect::<Vec<&ModSpecification>>();
+
+    println!("fetching mods...");
+    let paths = state.store.fetch_mods(&urls, update).await?;
+
+    integrate::integrate(path_game, to_integrate.into_iter().zip(paths).collect())
+}
+
+pub async fn resolve_and_integrate_with_provider_init<P, F>(
+    path_game: P,
+    state: &mut State,
+    mod_specs: &[ModSpecification],
+    update: bool,
+    init: F,
+) -> Result<()>
+where
+    P: AsRef<Path>,
+    F: Fn(&mut State, ModSpecification, &ProviderFactory) -> Result<()>,
+{
+    loop {
+        match resolve_and_integrate(&path_game, &state, &mod_specs, update).await {
+            Ok(()) => return Ok(()),
+            Err(e) => match e.downcast::<IntegrationError>() {
+                Ok(IntegrationError::NoProvider { spec, factory }) => init(state, spec, factory)?,
+                Err(e) => return Err(e),
+            },
+        }
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,13 @@
-use std::collections::HashSet;
 use std::path::PathBuf;
 
 use anyhow::{Context, Result};
 use clap::{Parser, Subcommand};
 
 use drg_mod_integration::{
-    error::IntegrationError,
     find_drg,
     gui::gui,
-    providers::ResolvableStatus,
-    providers::{ModResolution, ModSpecification},
+    providers::{ModSpecification, ProviderFactory}, resolve_and_integrate_with_provider_init,
     state::State,
-    integrate,
 };
 
 /// Command line integration tool.
@@ -36,6 +32,21 @@ struct ActionIntegrate {
     mods: Vec<String>,
 }
 
+/// Integrate a profile
+#[derive(Parser, Debug)]
+struct ActionIntegrateProfile {
+    /// Path to the "Deep Rock Galactic" installation directory. Only necessary if it cannot be found automatically.
+    #[arg(short, long)]
+    drg: Option<PathBuf>,
+
+    /// Update mods. By default all mods and metadata are cached offline so this is necessary to check for updates.
+    #[arg(short, long)]
+    update: bool,
+
+    /// Paths of mods to integrate
+    profile: String,
+}
+
 /// Work in progress GUI. Not usable yet.
 #[derive(Parser, Debug)]
 struct ActionGui {}
@@ -43,6 +54,7 @@ struct ActionGui {}
 #[derive(Subcommand, Debug)]
 enum Action {
     Integrate(ActionIntegrate),
+    Profile(ActionIntegrateProfile),
     Gui(ActionGui),
 }
 
@@ -64,6 +76,10 @@ fn main() -> Result<()> {
             action_integrate(action).await?;
             Ok(())
         }),
+        Action::Profile(action) => rt.block_on(async {
+            action_integrate_profile(action).await?;
+            Ok(())
+        }),
         Action::Gui(action) => {
             std::thread::spawn(move || {
                 rt.block_on(std::future::pending::<()>());
@@ -72,6 +88,31 @@ fn main() -> Result<()> {
             Ok(())
         }
     }
+}
+
+fn init_provider(
+    state: &mut State,
+    spec: ModSpecification,
+    factory: &ProviderFactory,
+) -> Result<()> {
+    println!("Initializing provider for {:?}", spec);
+    let params = state
+        .config
+        .provider_parameters
+        .entry(factory.id.to_owned())
+        .or_default();
+    for p in factory.parameters {
+        if !params.contains_key(p.name) {
+            // this blocks but since we're calling it on the main thread it'll be fine
+            let value =
+                dialoguer::Password::with_theme(&dialoguer::theme::ColorfulTheme::default())
+                    .with_prompt(p.description)
+                    .interact()
+                    .unwrap();
+            params.insert(p.id.to_owned(), value);
+        }
+    }
+    state.store.add_provider(factory, params)
 }
 
 async fn action_integrate(action: ActionIntegrate) -> Result<()> {
@@ -83,93 +124,42 @@ async fn action_integrate(action: ActionIntegrate) -> Result<()> {
 
     let mod_specs = action
         .mods
-        .iter()
-        .map(|url| ModSpecification {
-            url: url.to_owned(),
-        })
+        .into_iter()
+        .map(|url| ModSpecification { url })
         .collect::<Vec<_>>();
 
-    let mods = loop {
-        match state.store.resolve_mods(&mod_specs, action.update).await {
-            Ok(mods) => break mods,
-            Err(e) => match e.downcast::<IntegrationError>() {
-                Ok(IntegrationError::NoProvider { spec, factory }) => {
-                    println!("Initializing provider for {:?}", spec);
-                    let params = state
-                        .config
-                        .provider_parameters
-                        .entry(factory.id.to_owned())
-                        .or_default();
-                    for p in factory.parameters {
-                        if !params.contains_key(p.name) {
-                            let value = dialoguer::Password::with_theme(
-                                &dialoguer::theme::ColorfulTheme::default(),
-                            )
-                            .with_prompt(p.description)
-                            .interact()
-                            .unwrap();
-                            params.insert(p.id.to_owned(), value);
-                        }
-                    }
-                    state.store.add_provider(factory, params)?;
-                }
-                Err(e) => return Err(e),
-            },
-        }
-    };
-
-    println!("resolvable mods:");
-    for m in &mod_specs {
-        if let ResolvableStatus::Resolvable(resolution) = &mods[m].status {
-            println!("{:?}", resolution);
-        }
-    }
-
-    let mods_set = mod_specs
-        .iter()
-        .flat_map(|m| match &mods[m].status {
-            ResolvableStatus::Resolvable(ModResolution { url }) => Some(url),
-            _ => None,
-        })
-        .collect::<HashSet<_>>();
-
-    let missing_deps = mod_specs
-        .iter()
-        .flat_map(|m| {
-            mods[m]
-                .suggested_dependencies
-                .iter()
-                .filter_map(|m| match &mods[m].status {
-                    ResolvableStatus::Resolvable(ModResolution { url }) => {
-                        (!mods_set.contains(url)).then_some(url)
-                    }
-                    _ => Some(&m.url),
-                })
-        })
-        .collect::<HashSet<_>>();
-    if !missing_deps.is_empty() {
-        println!("WARNING: The following dependencies are missing:");
-        for d in missing_deps {
-            println!("  {d}");
-        }
-    }
-
-    let to_integrate = mod_specs
-        .iter()
-        .map(|u| mods[u].clone())
-        .collect::<Vec<_>>();
-    let urls = to_integrate
-        .iter()
-        .map(|m| &m.spec) // TODO this should be a ModResolution not a ModSpecification, we're missing a step here
-        .collect::<Vec<&ModSpecification>>();
-
-    println!("fetching mods...");
-    let paths = state.store.fetch_mods(&urls, action.update).await?;
-
-    integrate::integrate(
+    resolve_and_integrate_with_provider_init(
         path_game,
-        to_integrate.into_iter().zip(paths).collect(),
+        &mut state,
+        &mod_specs,
+        action.update,
+        init_provider,
     )
+    .await
+}
+
+async fn action_integrate_profile(action: ActionIntegrateProfile) -> Result<()> {
+    let path_game = action.drg.or_else(find_drg).context(
+        "Could not find DRG install directory, please specify manually with the --drg flag",
+    )?;
+
+    let mut state = State::new()?;
+    let profile = &state.profiles.profiles[&action.profile];
+
+    let mod_specs = profile
+        .mods
+        .iter()
+        .map(|config| config.spec.clone())
+        .collect::<Vec<_>>();
+
+    resolve_and_integrate_with_provider_init(
+        path_game,
+        &mut state,
+        &mod_specs,
+        action.update,
+        init_provider,
+    )
+    .await
 }
 
 fn action_gui(_action: ActionGui) -> Result<()> {
